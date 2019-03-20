8500a881836c10d53a3551cd01c9a02797398dd5
RBK-6 <rbk-6@RBK-6s-Mac-mini.Dlink>
2019-03-20 09:47:34 +0200

    solu

--- strings.js
+++ strings.js
@@ -3,5 +3,17 @@
 // repeatStr("hello",4) ==> 'hellohellohellohello'
 	
 	function repeatStr (n, s) {
-		//your code is here
+		
+     var string = "";
+
+      while (n > 0) { 
+ 
+      string += s; 
+       n--; 
+  }
+  
+  return string; 
+}
+
+repeatStr("hello", 3);//your code is here
 	}
